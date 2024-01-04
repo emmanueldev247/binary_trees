7e926900c8497c1fddbe135cce676480dcfccd96
emmanueldev247 <mailemmydee@gmail.com>
2024-01-04 15:49:52 +0100

    updated: bug fixed

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -4,7 +4,7 @@
  * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: pointer to the root node of the tree to count the number of leaves
  *
- * Return: 0 if the tree id NULL
+ * Return: 0 if the tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,10 +9,13 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1);
+		return (0);
 	if ((tree->left == NULL && tree->right != NULL)
 		|| (tree->left != NULL && tree->right == NULL))
 		return (0);
+	else if (tree->left == NULL && tree->right == NULL)
+		return (1);
 	else
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
 }
