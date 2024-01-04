119612a5af247e1af8752dae33c608f9d3776668
emmanueldev247 <mailemmydee@gmail.com>
2024-01-04 01:14:15 +0100

    updated: used new_node as parent of the old node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,7 +27,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	old_node = parent->left;
 	parent->left = new_node;
 	if (old_node)
+	{
 		new_node->left = old_node;
+		old_node->parent = new_node;
+	}
 
 	return (new_node);
 }
